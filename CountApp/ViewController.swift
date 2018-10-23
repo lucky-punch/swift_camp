cbe0cf6eb4ed331c6e02f1ae3ba35b73abf76dbe
suzutaka <ess.discussion@gmail.com>
2018-10-23 14:09:38 +0900

    課題1(CountApp)制作完了

--- CountApp/ViewController.swift
+++ CountApp/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number: Int! = 0
+    
+    @IBOutlet var countLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func plusButton(_ sender: Any) {
+        number += 1
+        countLabel.text = String(number)
+    }
+    
+    @IBAction func ACButton(_ sender: Any) {
+        number *= 0
+        countLabel.text = String(number)
+        
+    }
+        @IBAction func minusButton(_ sender: Any) {
+        number -= 1
+        countLabel.text = String(number)
+    }
 }
 
